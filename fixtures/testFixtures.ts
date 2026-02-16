76af6b6aa58abea6f014a76e327c9b45e703f31d
lcnburlacu <lcnburlacu@gmail.com>
2026-02-16 17:46:19 +0200

    Basic locators, add/remove buttons, use test.step

--- fixtures/testFixtures.ts
+++ fixtures/testFixtures.ts
@@ -1,9 +1,11 @@
 import { test as base } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
+import { CommonLocatorsPage } from '../pages/CommonLocatorsPage';
 
 // Define the types for your fixtures
 type MyFixtures = {
   loginPage: LoginPage;
+  commonLocatorsPage: CommonLocatorsPage;
 };
 
 // Extend the base test to include your Page Object
@@ -12,6 +14,13 @@ export const test = base.extend<MyFixtures>({
     const loginPage = new LoginPage(page);
     await use(loginPage);
   },
+
+    commonLocatorsPage: async ({page}, use) => {
+    const commonLocatorsPage = new CommonLocatorsPage(page);
+    await use(commonLocatorsPage);
+    }
 });
+
+
 
 export { expect } from '@playwright/test';
---
+++ pages/CommonLocatorsPage.ts
@@ -0,0 +1,37 @@
+import {Page, Locator} from "@playwright/test"
+import {BasePage} from "./BasePage"
+
+export class CommonLocatorsPage extends BasePage{
+
+    readonly addElement: Locator;
+    readonly deleteElement: Locator;
+    readonly headingText: Locator;
+
+    constructor(page: Page) {
+        super(page)
+        this.addElement = page.getByRole("button", {name: "Add Element"})
+        this.deleteElement = page.getByRole("button", {name: "Delete"})
+        this.headingText  = page.getByRole("heading", {name: "Add/Remove Elements"})
+    }
+
+    async gotoAddRemoveElements(): Promise<void>{
+        await this.navigate('https://the-internet.herokuapp.com/add_remove_elements/')
+    }
+
+    async addNewElement(): Promise<void> {
+        await this.addElement.click();
+  }
+
+    async addMultipleDeleteButtons(n: number): Promise<void> {
+        for (let i=0; i<n; i++){
+            await this.addNewElement();
+        }
+    }
+
+    async deleteAllButtons(): Promise<void>{
+        const buttonCount = await this.deleteElement.count()
+        for(let i=0; i<buttonCount; i++){
+            await this.deleteElement.first().click()
+        }
+    }
+}
