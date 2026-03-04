84789e73545918494a1a027c3cf77a51e8d670e9
lcnburlacu <lcnburlacu@gmail.com>
2026-03-04 16:10:01 +0200

    fix users tests

--- tests/api/functional/users.api.spec.ts
+++ tests/api/functional/users.api.spec.ts
@@ -6,24 +6,24 @@ test.describe('Users API Functional Tests', () => {
 
   test('GET /users returns 200', async ({ usersClient }) => {
     const response = await usersClient.getAllUsers();
-    expect (response.status).toBe(200)
+    expect (response.status()).toBe(200)
     await assert.assertNonEmptyArray(response);
   });
 
   test('GET /users/:id returns user', async ({ usersClient }) => {
     const response = await usersClient.getUserById(1);
-    expect (response.status).toBe(200)
+    expect (response.status()).toBe(200)
     await assert.assertUserId(response, 1);
     await assert.assertUserHasEmail(response);
   });
 
   test('GET invalid user returns 404', async ({ usersClient }) => {
     const response = await usersClient.getUserById(9999);
-    expect (response.status).toBe(404)
+    expect (response.status()).toBe(404)
   });
 
   test('POST /users creates user', async ({ usersClient }) => {
     const response = await usersClient.createUser({ name: 'John', username: 'snow', email: 'john@snow.north' });
-    expect (response.status).toBe(201)
+    expect (response.status()).toBe(201)
   });
 });
